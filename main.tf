f65b9fba7aab4156c2626f2ceb7fc98a5b0393f0
abhishekdontukurti <dabhishek175@gmail.com>
2026-04-10 22:35:54 -0400

    Terraform v4

--- main.tf
+++ main.tf
@@ -27,3 +27,21 @@ variable "components"{
     analytics-service = ""
   }
 }
+resource "null_resource" "ansible" {
+
+  depends_on = [aws_route53_record.dns]
+
+  for_each = var.components
+  provisioner "remote-exec" {
+    connection {
+      type = "ssh"
+      host = aws_instance.instance[each.key].public_ip
+      user = "ec2-user"
+      password = "DevOps321"
+    }
+    inline = [
+      "sudo labauto ansible",
+      "ansible-pull -i localhost, -U https://github.com/raghudevopsb88/wmp-ansible-v4.git main.yml -e env=dev -e COMPONENT=${each.key}"
+    ]
+  }
+}
